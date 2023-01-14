1a9d9069690fcc1ef02c5e57890836713d35db11
JEdmario16 <jedmario068@gmail.com>
2023-01-14 16:36:36 -0300

    first commit

---
+++ libs/macro_tools.py
@@ -0,0 +1,10 @@
+from win32gui import GetWindowText, GetForegroundWindow
+import mss
+from PIL import Image
+import os
+import configparser
+import pyautogui
+import webbrowser
+import keyboard
+from time import sleep
+
